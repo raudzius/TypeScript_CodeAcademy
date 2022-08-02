f8dc960ad8d10b36979f103f75f5fcfa1c752c8c
Mykolas Raudzius <raudziusmykolas@gmail.com>
2022-08-02 19:23:10 +0300

    feat(1): init and configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [0],
+  },
+};
