24721b689f7597b5a5fbe2b3588a837154066b95
Mykolas Raudzius <raudziusmykolas@gmail.com>
2022-08-02 18:26:36 +0300

    feat(1): console.group 1st task

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
 "use strict";
+console.group('typescript/eslint testas');
 console.log('Hello World');
+console.groupEnd();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,3 @@
+console.group('typescript/eslint testas');
 console.log('Hello World');
+console.groupEnd();
