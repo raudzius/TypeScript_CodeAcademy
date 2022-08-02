6c2a8d1c4f07a1f680a63d1c9d01a9e176d21500
Mykolas Raudzius <raudziusmykolas@gmail.com>
2022-08-02 18:43:08 +0300

    feat(1): complete task 5

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,24 @@
 "use strict";
-console.group('typescript/eslint testas');
-console.log('Hello World');
-console.groupEnd();
+const init = () => {
+    console.group('typescript/eslint testas');
+    console.log('Hello World');
+    console.groupEnd();
+    console.group('task 5');
+    const firstName = 'Mykolas';
+    const age = 21;
+    const coins = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2];
+    const products = [
+        { title: 'Milk', price: 2 },
+        { title: 'Apple', price: 0.3 },
+        { title: 'Jeans', price: 50 },
+    ];
+    const friends = [
+        { name: 'John', age: 27, status: 'divorced' },
+        { name: 'Gary', age: 28, status: 'married' },
+        { name: 'Lilly', age: 40, status: 'widow' },
+    ];
+    console.log(firstName, age, coins, products, friends);
+    console.groupEnd();
+};
+init();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,36 @@
-console.group('typescript/eslint testas');
-console.log('Hello World');
-console.groupEnd();
+const init = () => {
+  console.group('typescript/eslint testas');
+  console.log('Hello World');
+  console.groupEnd();
+
+  console.group('task 5');
+  const firstName: string = 'Mykolas';
+  const age: number = 21;
+  const coins: number[] = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2];
+
+  type Product = { title: string; price: number };
+
+  const products: Product[] = [
+    { title: 'Milk', price: 2 },
+    { title: 'Apple', price: 0.3 },
+    { title: 'Jeans', price: 50 },
+  ];
+
+  interface Person {
+    name: string;
+    age: number;
+    status: 'married' | 'divorced' | 'widow';
+  }
+
+  const friends: Person[] = [
+    { name: 'John', age: 27, status: 'divorced' },
+    { name: 'Gary', age: 28, status: 'married' },
+    { name: 'Lilly', age: 40, status: 'widow' },
+  ];
+
+  console.log(firstName, age, coins, products, friends);
+
+  console.groupEnd();
+};
+
+init();
