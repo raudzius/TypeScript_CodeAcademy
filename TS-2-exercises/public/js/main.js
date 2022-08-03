4c719d3e91a17fc1a98f0651e87a560ff733c591
Mykolas Raudzius <raudziusmykolas@gmail.com>
2022-08-03 16:37:42 +0300

    feat(2): complete drink sorting

--- TS-2-exercises/public/js/main.js
+++ TS-2-exercises/public/js/main.js
@@ -1,10 +1,13 @@
 "use strict";
-console.groupCollapsed('1 - https://edabit.com/challenge/nuXdWHAoHv9y38sn7');
+console.groupCollapsed('1 - https://edabit.com/challenge/nuXdWHAoHv9y38sn7 ');
 {
-}
-console.groupEnd();
-console.groupCollapsed('2 - https://edabit.com/challenge/9KEKJG5PZTFmG3Zau');
-{
+    const drinks = [
+        { name: 'lemonade', price: 50 },
+        { name: 'lime', price: 10 },
+        { name: 'coke', price: 15 },
+    ];
+    const sortDrinks = (drinkArray) => [...drinkArray].sort((a, b) => a.price - b.price);
+    console.log(sortDrinks(drinks));
 }
 console.groupEnd();
 //# sourceMappingURL=main.js.map
--- TS-2-exercises/src/main.ts
+++ TS-2-exercises/src/main.ts
@@ -1,12 +1,24 @@
-console.groupCollapsed('1 - https://edabit.com/challenge/nuXdWHAoHv9y38sn7');
+/* eslint-disable no-lone-blocks */
+console.groupCollapsed('1 - https://edabit.com/challenge/nuXdWHAoHv9y38sn7 ');
 {
+    type Drink = { name: string, price: number };
 
+    const drinks: Drink[] = [
+      { name: 'lemonade', price: 50 },
+      { name: 'lime', price: 10 },
+      { name: 'coke', price: 15 },
+    ];
+
+    const sortDrinks = (drinkArray: Drink[]) => [...drinkArray].sort(
+      (a: Drink, b: Drink) => a.price - b.price,
+    );
+
+    console.log(sortDrinks(drinks));
 }
 console.groupEnd();
 
-console.groupCollapsed('2 - https://edabit.com/challenge/9KEKJG5PZTFmG3Zau');
-{
+// console.groupCollapsed('2 - https://edabit.com/challenge/9KEKJG5PZTFmG3Zau ');
+// // {
 
-}
-console.groupEnd();
-
+// // }
+// console.groupEnd();
