a6ff019d3d2442b296f1ca680664ab60d8e61049
Mykolas Raudzius <raudziusmykolas@gmail.com>
2022-08-03 14:40:34 +0300

    feat(2): ts file compilation

---
+++ TS-2-exercises/public/js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+console.groupCollapsed('1 - https://edabit.com/challenge/nuXdWHAoHv9y38sn7');
+{
+}
+console.groupEnd();
+console.groupCollapsed('2 - https://edabit.com/challenge/9KEKJG5PZTFmG3Zau');
+{
+}
+console.groupEnd();
+//# sourceMappingURL=main.js.map
