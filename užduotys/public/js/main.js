45ae5b900bfcbd7e4e51525580b3b09ccfed0b90
Mykolas Raudzius <raudziusmykolas@gmail.com>
2022-08-04 16:48:45 +0300

    init(3): typing practise

---
+++ užduotys/public/js/main.js
@@ -0,0 +1,66 @@
+"use strict";
+console.group('1. Parašykite funkciją, kuri atspausdina paskutinio masyvo elemento indeksą');
+{
+}
+console.groupEnd();
+console.group('2. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indeksus eilutėmis');
+{
+}
+console.groupEnd();
+console.group('3. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento reikšmes eilutėmis');
+{
+}
+console.groupEnd();
+console.group('4. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indekso ir reikšmių poras eilutėmis, tokiu formatu:  ');
+{
+}
+console.groupEnd();
+console.group('5. Parašykite funkciją, kuri atspausdina masyvo elementus atbuline tvarka eilutėmis, iš galo.');
+{
+}
+console.groupEnd();
+console.group('6. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indeksus vienoje eilutėje: 0 1 2 3 ...');
+{
+}
+console.groupEnd();
+console.group('7. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento reikšmes vienoje eilutėje: -111 2 -9 48 ...');
+{
+}
+console.groupEnd();
+console.group('8. Parašykite funkciją, kuri atspausdina kiekvieno masyvo elemento indekso ir reikšmių poras vienoje eilutėje, tokiu formatu:');
+{
+}
+console.groupEnd();
+console.group('9. Sukurkite funkciją, kuri priima skaičių masyvą ir grąžina naują masyvą sudarytą iš pradinio masyvo elementų padaugintų iš 2');
+{
+}
+console.groupEnd();
+console.group('10. Sukurkite funkciją, kuri priima skaičių masyvą ir grąžina naują masyvą sudarytą iš pradinio masyvo elementų pakeltų kvadratu');
+{
+}
+console.groupEnd();
+console.group('11. Sukurkite funkciją, kuri priima skaičių masyvą ir grąžina naują masyvą sudarytą iš pradinio masyvo elementų ir jų indeksų sandaugos');
+{
+}
+console.groupEnd();
+console.group('12. Sukurkite funkciją, kuri priima skaičių masyvą ir grąžina naują masyvą sudarytą iš pradinio masyvo teigiamų elementų');
+{
+}
+console.groupEnd();
+console.group('13. Sukurkite funkciją, kuri priima skaičių masyvą ir grąžina naują masyvą sudarytą iš pradinio masyvo NE-teigiamų elementų');
+{
+}
+console.groupEnd();
+console.group('14. Sukurkite funkciją, kuri priima skaičių masyvą ir grąžina naują masyvą sudarytą iš pradinio masyvo lyginių elementų');
+{
+}
+console.groupEnd();
+console.group('15. Sukurkite funkciją, kuri priima skaičių masyvą ir grąžina naują masyvą sudarytą iš pradinio masyvo nelyginių elementų');
+{
+}
+console.groupEnd();
+console.group('16. Sukurkite funkciją, kuri priima skaičių masyvą ir grąžina naują masyvą sudarytą iš pradinio masyvo, paverstų teigiamais, elementų');
+{
+}
+console.groupEnd();
+//# sourceMappingURL=main.js.map
